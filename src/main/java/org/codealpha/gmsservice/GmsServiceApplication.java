a9e5ed38729fcd7b00f05c5f6640211aa10faf9c
Anuj Bhatnagar <developer@enstratify.com>
2019-03-12 15:02:10 +0530

    Initial Commit

---
+++ src/main/java/org/codealpha/gmsservice/GmsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.codealpha.gmsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsServiceApplication.class, args);
+	}
+
+}
