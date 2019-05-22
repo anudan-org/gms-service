990ebefa04b83ec73c035da51be5597fef56590b
apple <apple@apples-MacBook-Pro.local>
2019-05-22 18:07:24 +0530

    Changed UT runner

--- src/test/java/org/codealpha/gmsservice/services/AppConfigServiceTest.java
+++ src/test/java/org/codealpha/gmsservice/services/AppConfigServiceTest.java
@@ -12,9 +12,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class AppConfigServiceTest {
 
--- src/test/java/org/codealpha/gmsservice/services/GranteeServiceTest.java
+++ src/test/java/org/codealpha/gmsservice/services/GranteeServiceTest.java
@@ -17,7 +17,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class GranteeServiceTest {
 
