4b2bb0a62ac5e250d4600f3944f3cc7f5ef88b00
Ranjit Victor <ranjit@socialalpha.org>
2020-07-06 17:23:49 +0530

    Fixed bug in mail logging table creation script
    Former-commit-id: 1c07e2a4037f3e406fed693b0e28c44df379904e

--- DB/2020-07-06.sql
+++ DB/2020-07-06.sql
@@ -3,9 +3,9 @@
 create sequence mail_id_seq start 1;
 create table mail_logs(
     id bigint not null default nextval('mail_id_seq'::regclass),
-    sent_on timestamp;
+    sent_on timestamp,
     cc text,
-    sent_to
+    sent_to text,
     msg text,
     subject text,
     status boolean,
