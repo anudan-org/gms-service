0ffd23940b5c1712bac8975656095108c25a7fa4
Ranjit Victor <ranjit@socialalpha.org>
2020-07-16 08:38:16 +0530

    Testing commits for changelog extraction
    Former-commit-id: 14777ade746c5fcc5bc5ad8a0aa62f15598db3a0

--- src/main/java/org/codealpha/gmsservice/services/DashboardService.java
+++ src/main/java/org/codealpha/gmsservice/services/DashboardService.java
@@ -25,6 +25,8 @@ import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//Aded comment for testing
+
 @Service
 public class DashboardService {
 
