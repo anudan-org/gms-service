b9a5ee7e1d68a08c28ac2f607e493d8412bb2862
Ranjit Victor <ranjit@socialalpha.org>
2020-07-26 10:37:53 +0530

    Functionality for user re-invite
    Former-commit-id: a4777b7373a77098c4c92e3609925903506bf76a

---
+++ src/main/java/org/codealpha/gmsservice/models/NewRoleModel.java
@@ -0,0 +1,15 @@
+package org.codealpha.gmsservice.models;
+
+public class NewRoleModel {
+
+    private String roleName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+}
