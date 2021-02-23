0bc6102e0710d206472b3b075ba8075866c5520c
Ranjit Victor <ranjit@socialalpha.org>
2021-02-23 10:07:05 +0530

    Creating tenant file for ssl securing
    Former-commit-id: 6a6ea712cd9ff2c0af4b7c12918e84419d7b447a

--- src/main/java/org/codealpha/gmsservice/controllers/GranterController.java
+++ src/main/java/org/codealpha/gmsservice/controllers/GranterController.java
@@ -3,6 +3,9 @@ package org.codealpha.gmsservice.controllers;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,6 +14,7 @@ import java.util.List;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.FileUtils;
 import org.codealpha.gmsservice.constants.WorkflowObject;
 import org.codealpha.gmsservice.entities.*;
 import org.codealpha.gmsservice.repositories.GranterRepository;
@@ -132,6 +136,14 @@ public class GranterController {
 		org.setName(granterName);
 		org.setOrganizationType("GRANTER");
 		org = granterService.createGranter((Granter) org, image);
+
+		File file = new File("/opt/gms/secure-sites.txt");
+		try{
+
+			Files.write(file.toPath(),System.lineSeparator().concat(slug.toLowerCase()).concat(".").getBytes(), StandardOpenOption.APPEND);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		try {
 			String filePath = uploadLocation + slug.toUpperCase() + "/logo/";
