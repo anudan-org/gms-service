9823e9b3e48287c6d3596e4e86803479ff912524
Ranjit Victor <ranjit@socialalpha.org>
2021-04-28 19:58:39 +0530

    Fixed logic for retrieving disabled users to be notified
    Former-commit-id: f95c3489cde667079b5e77255813ec8052e87b35

--- src/main/java/org/codealpha/gmsservice/repositories/DisabledUsersEntityRepository.java
+++ src/main/java/org/codealpha/gmsservice/repositories/DisabledUsersEntityRepository.java
@@ -14,7 +14,7 @@ public interface DisabledUsersEntityRepository extends CrudRepository<DisabledUs
             "inner join grants F on F.id=C.grant_id\n" +
             "inner join workflow_statuses D on D.id=C.status_id\n" +
             "inner join organizations E on E.id=B.organization_id\n" +
-            "where B.deleted=true and D.internal_status!='CLOSED' and E.organization_type!='GRANTEE'",nativeQuery = true)
+            "where B.deleted=true and D.internal_status!='CLOSED' and E.organization_type!='GRANTEE' and F.deleted=false",nativeQuery = true)
     List<DisabledUsersEntity> getReports();
 
     @Query(value = "select distinct C.id, 'Disbursement Approval Request for ' entity_type,F.name entity_name from disbursement_assignments A \n" +
@@ -23,14 +23,18 @@ public interface DisabledUsersEntityRepository extends CrudRepository<DisabledUs
             "inner join grants F on F.id=C.grant_id\n" +
             "inner join workflow_statuses D on D.id=C.status_id\n" +
             "inner join organizations E on E.id=B.organization_id\n" +
-            "where B.deleted=true and D.internal_status!='CLOSED' and E.organization_type!='GRANTEE'",nativeQuery = true)
+            "where B.deleted=true and D.internal_status!='CLOSED' and E.organization_type!='GRANTEE' and F.deleted=false",nativeQuery = true)
     List<DisabledUsersEntity> getDisbursements();
 
-    @Query(value = "select distinct C.id, 'Grant' entity_type, C.name entity_name from grant_assignments A \n" +
-            "inner join users B on B.id=A.assignments\n" +
-            "inner join grants C on C.id=A.grant_id\n" +
-            "inner join workflow_statuses D on D.id=C.grant_status_id\n" +
-            "inner join organizations E on E.id=B.organization_id\n" +
-            "where B.deleted=true and D.internal_status!='CLOSED' and E.organization_type!='GRANTEE'",nativeQuery = true)
+    @Query(value = "select distinct C.id, 'Grant' entity_type, C.name entity_name from grant_assignments A\n" +
+            "            inner join users B on B.id=A.assignments\n" +
+            "            inner join grants C on C.id=A.grant_id\n" +
+            "            inner join workflow_statuses D on D.id=C.grant_status_id\n" +
+            "            inner join organizations E on E.id=B.organization_id\n" +
+            "            where B.deleted=true and D.internal_status!='CLOSED'\n" +
+            "\t\t\tand( (D.internal_status='DRAFT' AND (select count(*) from grant_history where id=C.id)>0)\n" +
+            "\t\t\tor D.internal_status='REVIEW' or D.internal_status='ACTIVE'\n" +
+            "\t\t\t   )\n" +
+            "\t\t\tand E.organization_type!='GRANTEE' and C.deleted=false",nativeQuery = true)
     List<DisabledUsersEntity> getGrants();
 }
