8311e95e3969c55d4969fed463d961c9a578cb0d
Ranjit Victor <ranjit@socialalpha.org>
2021-06-08 11:16:40 +0530

    New queries for workflow validations
    Former-commit-id: 84b067123558d82a3a01d477e7fcbfafd942b5db

--- DB/2021-05-31.sql
+++ DB/2021-05-31.sql
@@ -63,7 +63,41 @@ alter table workflow_validations add column message_id bigint;
 alter table grants drop constraint fkfxhc0yhlrne4obtxvc11skonn;
 alter table grant_history drop constraint fkfxhc0yhlrne4obtxvc11skonn;
 
+INSERT INTO public.messages (id, message) VALUES (1, 'Planned Disbursement of Project Funds is not equal to the Grant Amount of %grantamount%');
+INSERT INTO public.messages (id, message) VALUES (2, '%_for% has missing header information.');
+INSERT INTO public.messages (id, message) VALUES (3, 'There are no Planned Funds for this project');
+INSERT INTO public.messages (id, message) VALUES (4, 'Requested amount cannot be greater than project''s undisbursed amount');
+INSERT INTO public.messages (id, message) VALUES (5, 'The disbursed total cannot be greater than the approved amount of %requested_amount%');
+INSERT INTO public.workflow_validations (id, object, validation_query, type, active, message_id) VALUES (2, 'GRANT', 'select case when amount is null or amount=0 or representative is null or representative='''' or name is null or name='''' or organization_id is null or start_date is null or end_date is null then true else false end _failed,''GRANT'' _for
+from grants where id=%grantId%;', 'WARN', true, 2);
+INSERT INTO public.workflow_validations (id, object, validation_query, type, active, message_id) VALUES (3, 'GRANT', 'select not exists(select  from grant_specific_section_attributes a
+inner join grant_string_attributes b on b.section_attribute_id=a.id
+where b.grant_id=%grantId% and a.field_type=''disbursement'' group by b.grant_id) _failed;
+', 'WARN', true, 3);
+INSERT INTO public.workflow_validations (id, object, validation_query, type, active, message_id) VALUES (1, 'GRANT', 'select case when grantamount!=sum(_value::double precision) or grantamount =0 then true else false end _failed,grantamount from (select _name, case when grantamount is null or grantamount=0 then 0 else grantamount end,cast ((case when X._value is null or X._value='''' then ''0'' else X._value end) as double precision) from (select
+c.amount grantamount,
+json_extract_path_text(json_array_elements(json_extract_path(json_array_elements(a.value::json),''columns'')),''name'') _name,
+json_extract_path_text(json_array_elements(json_extract_path(json_array_elements(a.value::json),''columns'')),''value'') _value from grant_string_attributes a
+inner join grant_specific_section_attributes b on b.id=a.section_attribute_id
+inner join grants c on c.id=a.grant_id
+where b.field_type=''disbursement'' and a.grant_id=%grantId%) X where X._name=''Amount'') Y group by _name,grantamount;
+', 'WARN', true, 1);
+INSERT INTO public.workflow_validations (id, object, validation_query, type, active, message_id) VALUES (4, 'REPORT', 'select case when name is null or name='''' or start_date is null or end_date is null or due_date is null then true else false end _failed,''REPORT'' _for from reports where id=%reportId%;', 'WARN', true, 2);
+INSERT INTO public.workflow_validations (id, object, validation_query, type, active, message_id) VALUES (5, 'DISBURSEMENT', 'select case when requested_amount>(b.amount-disbursed_amount_for_grant(b.id)) then true else false end _failed,(b.amount-disbursed_amount_for_grant(b.id)) _undisbursed  from disbursements a
+left join actual_disbursements c on c.disbursement_id = a.id
+inner join grants b on b.id=a.grant_id
+where a.id=%disbursementId% ', 'WARN', true, 4);
+INSERT INTO public.workflow_validations (id, object, validation_query, type, active, message_id) VALUES (6, 'DISBURSEMENT', 'select case when %amount_to_record%>requested_amount then true else false end _failed,requested_amount from disbursements where id=%disbursementId%;', 'WARN', true, 5);
+
 -- FOR CINI MIGRATION
 update organizations set name=(select name from organizations where id=69) where id=11;
 
 update organizations set name='Collectives of Integrated Livelihood Initiatives' where id=11;
+---CINI POST MIGRATION - After a WEEK
+drop function cini_disbursement_status;
+drop function cini_grant_status;
+drop function cini_report_status;
+drop function get_owner_grant;
+drop function get_owner_report;
+drop function get_owner_disbursement;
+drop procedure migrate_cini;
