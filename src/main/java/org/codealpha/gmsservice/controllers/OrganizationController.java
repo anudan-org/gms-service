9c3cb7c68c45fda7a1f9e0b27bc2f39274b4df70
Ranjit Victor <ranjit@socialalpha.org>
2021-08-12 19:58:02 +0530

    org update api changes
    Former-commit-id: 8e01d4f3cdf2996c01d1f5b8c2773f6e672425f7

--- src/main/java/org/codealpha/gmsservice/controllers/OrganizationController.java
+++ src/main/java/org/codealpha/gmsservice/controllers/OrganizationController.java
@@ -7,7 +7,13 @@ import org.codealpha.gmsservice.services.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * @author Developer code-alpha.org
@@ -22,7 +28,8 @@ public class OrganizationController {
 
 	@Autowired
 	private OrganizationRepository repository;
-
+	@org.springframework.beans.factory.annotation.Value("${spring.upload-file-location}")
+	private String uploadLocation;
 
 
 	@GetMapping("/{organizationId}")
@@ -43,5 +50,25 @@ public class OrganizationController {
 		return org;
 	}
 
+	@PostMapping(value="/logo",consumes = {"multipart/form-data" })
+	public void saveOrganizationLogo(@RequestParam("file") MultipartFile image,
+											 @RequestHeader("X-TENANT-CODE") String tenantCode){
+		String filePath = uploadLocation + tenantCode + "/logo/";
+		File dir = new File(filePath);
+		dir.mkdirs();
 
+		File fileToCreate = new File(dir, "logo.png");
+
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(fileToCreate);
+			fos.write(image.getBytes());
+			fos.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
